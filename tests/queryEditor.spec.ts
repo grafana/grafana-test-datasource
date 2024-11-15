e7c20a69be31b48fdb8c0485f73c22d38e689525
Erik Sundell <erik.sundell87@gmail.com>
2024-11-15 07:46:42 +0100

    add smoke tests

--- tests/queryEditor.spec.ts
+++ tests/queryEditor.spec.ts
@@ -1,11 +1,8 @@
 import { test, expect } from '@grafana/plugin-e2e';
 
-test('data query should return a value', async ({ panelEditPage, readProvisionedDataSource }) => {
+test('Should render query editor', async ({ panelEditPage, readProvisionedDataSource }) => {
   const ds = await readProvisionedDataSource({ fileName: 'datasources.yml' });
   await panelEditPage.datasource.set(ds.name);
   await panelEditPage.setVisualization('Table');
-  await panelEditPage.getQueryEditorRow('A').getByRole('textbox', { name: 'Query Text' }).fill('test query');
-  await panelEditPage.getQueryEditorRow('A').getByRole('spinbutton').fill('10');
-  await expect(panelEditPage.panel.fieldNames).toContainText(['Time', 'Value']);
-  await expect(panelEditPage.panel.data).toContainText(['10']);
+  await expect(await panelEditPage.getQueryEditorRow('A').getByRole('textbox', { name: 'Query Text' })).toBeVisible();
 });
