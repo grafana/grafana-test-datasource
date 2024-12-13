bb598a142a5244f4f2af94cb6b3dc0cc17623de2
Erik Sundell <erik.sundell87@gmail.com>
2024-12-13 08:45:16 +0100

    add backend

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,20 +1,11 @@
-import { getBackendSrv, isFetchError } from '@grafana/runtime';
-import {
-  CoreApp,
-  DataQueryRequest,
-  DataQueryResponse,
-  DataSourceApi,
-  DataSourceInstanceSettings,
-  createDataFrame,
-  FieldType,
-} from '@grafana/data';
+import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
+import { CoreApp, DataSourceInstanceSettings, ScopedVars } from '@grafana/data';
 
-import { MyQuery, MyDataSourceOptions, DEFAULT_QUERY, DataSourceResponse } from './types';
-import { lastValueFrom } from 'rxjs';
+import { MyQuery, MyDataSourceOptions, DEFAULT_QUERY } from './types';
 import { VariableSupport } from 'variables';
 import { annotationSupport } from 'annotations';
 
-export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
+export class DataSource extends DataSourceWithBackend<MyQuery, MyDataSourceOptions> {
   baseUrl: string;
 
   constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
@@ -28,70 +19,15 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
     return DEFAULT_QUERY;
   }
 
+  applyTemplateVariables(query: MyQuery, scopedVars: ScopedVars) {
+    return {
+      ...query,
+      queryText: getTemplateSrv().replace(query.queryText, scopedVars),
+    };
+  }
+
   filterQuery(query: MyQuery): boolean {
     // if no query has been provided, prevent the query from being executed
     return !!query.queryText;
-  }
-
-  async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
-    const { range } = options;
-    const from = range!.from.valueOf();
-    const to = range!.to.valueOf();
-
-    // Return a constant for each query.
-    const data = options.targets.map((target) => {
-      return createDataFrame({
-        refId: target.refId,
-        fields: [
-          { name: 'Time', values: [from, to], type: FieldType.time },
-          { name: 'Value', values: [target.constant, target.constant], type: FieldType.number },
-        ],
-      });
-    });
-
-    return { data };
-  }
-
-  async request(url: string, params?: string) {
-    const response = getBackendSrv().fetch<DataSourceResponse>({
-      url: `${this.baseUrl}${url}${params?.length ? `?${params}` : ''}`,
-    });
-    return lastValueFrom(response);
-  }
-
-  /**
-   * Checks whether we can connect to the API.
-   */
-  async testDatasource() {
-    const defaultErrorMessage = 'Cannot connect to API';
-
-    try {
-      const response = await this.request('/health');
-      if (response.status === 200) {
-        return {
-          status: 'success',
-          message: 'Success',
-        };
-      } else {
-        return {
-          status: 'error',
-          message: response.statusText ? response.statusText : defaultErrorMessage,
-        };
-      }
-    } catch (err) {
-      let message = '';
-      if (typeof err === 'string') {
-        message = err;
-      } else if (isFetchError(err)) {
-        message = 'Fetch error: ' + (err.statusText ? err.statusText : defaultErrorMessage);
-        if (err.data && err.data.error && err.data.error.code) {
-          message += ': ' + err.data.error.code + '. ' + err.data.error.message;
-        }
-      }
-      return {
-        status: 'error',
-        message,
-      };
-    }
   }
 }
