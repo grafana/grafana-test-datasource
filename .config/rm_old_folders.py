94c05d8aae39f51fd55df7b769a926215fba599f
Erik Sundell <erik.sundell87@gmail.com>
2025-01-15 10:34:01 +0100

    update script

--- .config/rm_old_folders.py
+++ .config/rm_old_folders.py
@@ -4,16 +4,16 @@ import re
 from datetime import datetime, timedelta
 import shutil
 
-def find_old_folders(n_days, directory):
+def find_old_folders(retention_days, directory):
     """
-    Find folders in the specified directory that are older than n_days.
+    Find folders in the specified directory that are older than retention_days.
     
     Args:
         directory (str): The directory to search for folders.
-        n_days (int): The number of days to determine which folders to delete.
+        retentioretention_days (int): The number of days to determine which folders to delete.
 
     Returns:
-        list: List of folder names older than n_days.
+        list: List of folder names older than retention_days.
     """
     current_time = datetime.utcnow()
     folder_name_regex = re.compile(r'^\d{8}_\d{6}Z$')
@@ -24,10 +24,10 @@ def find_old_folders(n_days, directory):
             try:
                 folder_date = datetime.strptime(entry.name, "%Y%m%d_%H%M%SZ")
                 time_difference = current_time - folder_date
-                if time_difference > timedelta(days=n_days):
+                if time_difference > timedelta(days=retention_days):
                     old_folders.append(entry.name)
                 else:
-                    print(f"SKIPPED --- Folder '{entry.name}' is not older than {n_days}. It will not be deleted")
+                    print(f"SKIPPED --- Folder '{entry.name}' is not older than {retention_days}. It will not be deleted")
             except ValueError:
                 print(f"SKIPPED --- Error parsing timestamp for folder '{entry.name}'. It will not be deleted.")
         else:
@@ -55,9 +55,9 @@ def delete_folders(directory, folder_names):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Delete old folders in a specified directory.")
-    parser.add_argument("--n-days", type=int, required=True, help="Number of days (days older than current date) to determine which folders to delete.")
+    parser.add_argument("--retention-days", type=int, required=True, help="Number of days (days older than current date) to determine which folders to delete.")
     parser.add_argument("--folder-name", type=str, required=True, help="Full path to the directory where reports are located.")
     args = parser.parse_args()
 
-    old_folders = find_old_folders(args.n_days, args.folder_name)
+    old_folders = find_old_folders(args.retention_days, args.folder_name)
     delete_folders(args.folder_name, old_folders)
