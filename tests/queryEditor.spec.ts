482c310b4e2e720d3fe55102070868c2b0f07c89
sunker <sunker@users.noreply.github.com>
2025-01-21 08:03:34 +0000

    deploy: db8d9a5d03af633256a63a23649d40092bc07f83

--- tests/queryEditor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@grafana/plugin-e2e';
-
-test('should render query editor', async ({ panelEditPage, readProvisionedDataSource }) => {
-  const ds = await readProvisionedDataSource({ fileName: 'datasources.yml' });
-  await panelEditPage.datasource.set(ds.name);
-  await expect(panelEditPage.getQueryEditorRow('A').getByRole('textbox', { name: 'Query Text' })).toBeVisible();
-});
