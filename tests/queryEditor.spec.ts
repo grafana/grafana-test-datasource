94551b89e653971149d8a3f65cde9fcb9892877c
Erik Sundell <erik.sundell87@gmail.com>
2025-11-07 15:34:11 +0100

    use new workflow

--- tests/queryEditor.spec.ts
+++ tests/queryEditor.spec.ts
@@ -1,7 +1,8 @@
 import { test, expect } from '@grafana/plugin-e2e';
 
-test('should render query editor', async ({ panelEditPage, readProvisionedDataSource }) => {
+test('should render query editor', async ({ grafanaVersion, panelEditPage, readProvisionedDataSource }) => {
   const ds = await readProvisionedDataSource({ fileName: 'datasources.yml' });
   await panelEditPage.datasource.set(ds.name);
+  console.log(grafanaVersion);
   await expect(panelEditPage.getQueryEditorRow('A').getByRole('textbox', { name: 'Query Text' })).toBeVisible();
 });
