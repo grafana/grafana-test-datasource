8caf6a83f957ade946456110819d7d38dc7feb86
Erik Sundell <erik.sundell87@gmail.com>
2026-05-05 09:12:59 +0200

    test: remove vendored AxeA11yReporter scaffolding (trial complete)

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,10 +24,7 @@ export default defineConfig<PluginOptions>({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ['html'],
-    ['./tests/utils/axe-a11y/reporter.ts'],
-  ],
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
